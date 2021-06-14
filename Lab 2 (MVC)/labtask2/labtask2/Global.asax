c15b49dcd8cd93b60cbcf0f960bd2284dfd60125
Jahidh101 <19-39899-1@student.aiub.edu>
2021-06-14 23:23:17 +0600

    lab 2 added 1.0.0

---
+++ Lab 2 (MVC)/labtask2/labtask2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="labtask2.MvcApplication" Language="C#" %>
---
+++ Lab 2 (MVC)/labtask2/labtask2/Models/Database/Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace labtask2.Models.Database
+{
+    public class Database
+    {
+        public Students Students { get; set; }
+        public Database()
+        {
+            string connString = @"Server=DESKTOP-UM1IST6\SQLEXPRESS;Database=PMS;Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connString);
+
+            Students = new Students(conn);
+
+        }
+    }
+}
