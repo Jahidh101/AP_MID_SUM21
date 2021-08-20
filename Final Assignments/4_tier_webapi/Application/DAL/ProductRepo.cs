54e6b4a2b9162ee12873fe2905f160f92fd8d48c
Jahidh101 <19-39899-1@student.aiub.edu>
2021-08-20 19:38:12 +0600

    updated

---
+++ Final Assignments/4_tier_webapi/Application/DAL/ProductRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ProductRepo
+    {
+        static MFCSEntities context = new MFCSEntities();
+        public static List<Product> GetAllProducts()
+        {
+            
+            var p = context.Products.ToList();
+            return p;
+        }
+        public static List<Product_Type> GetProductTypeDetails()
+        {
+            return context.Product_Type.ToList();
+        }
+        public static List<Product_Type> GetAllProductType()
+        {
+            return context.Product_Type.ToList();
+        }
+        public static void AddProductType(Product_Type p)
+        {
+            context.Product_Type.Add(p);
+            context.SaveChanges();
+        }
+        public static void AddProduct(Product p)
+        {
+            context.Products.Add(p);
+            context.SaveChanges();
+        }
+        public static void AddBuy_Product(Buy_Products bp)
+        {
+            context.Buy_Products.Add(bp);
+            context.SaveChanges();
+        }
+        public static List<User> GetAllFarmerBuyer()
+        {
+            return context.Users.ToList();
+        }
+    }
+}
